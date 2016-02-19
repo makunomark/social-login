defab1ae8e80b201c2d3cc54700a969f03fe466b
makuno <makunomark@gmail.com>
2016-02-19 10:32:05 +0300

    refatoring whole project

---
+++ app/src/main/java/com/lsp/lumpyspace/LoginSample.java
@@ -0,0 +1,13 @@
+package com.lsp.lumpyspace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sample);
+    }
+}
